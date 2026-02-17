ed1daa09989df450643ba1176950e762c241ab71
MalikL2005 <your_email@example.com>
2026-02-17 23:03:51 +0100

    more unit tests

--- src/parser/tests/mod.rs
+++ src/parser/tests/mod.rs
@@ -3,7 +3,9 @@
 mod unit_tests {
     use std::collections::HashMap;
 
-    use crate::parser::{objects::{AddQO, QueryObject, AddNodeQO}, parse_query::parse_query};
+    use crate::parser::{errors::{ParseKeyValueError, ParseQueryError}, objects::{AddNodeQO, AddQO, QueryObject}, parse_query::parse_query};
+    use crate::parser::errors::ParseErrorReason::ParseKeyValuePairs;
+    use crate::parser::errors::ParseKeyValueErrorReason::MissingPropertyStr;
 
     use super::*;
     #[test]
@@ -30,8 +32,16 @@ mod unit_tests {
         let invalid_query = "ADD NODE TYPE".to_string();
         assert!(parse_query(invalid_query).is_err());
 
-        let invalid_query = "ADD NODE n1 TYPE PersonPROPERTIES name='Edos', age=20".to_string();
-        assert!(parse_query(invalid_query).is_err());
+        let invalid_query = "ADD NODE n1 TYPE PersonPROPERTIES name='Edos', age=20";
+        // type_name = "PersonPROPERTIES" => "PROPERTIES" is missing after type_name
+        let res = parse_query(invalid_query.to_string());
+        match res {
+            Err(ParseQueryError { 
+                reason: ParseKeyValuePairs(
+                    ParseKeyValueError { reason: r} ) 
+            }) => assert_eq!(r, MissingPropertyStr), 
+            _ => panic!("Expected query \"{invalid_query}\" to return an error, but it passed")
+        }
     }
 
 
